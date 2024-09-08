a728ed8b241b06afdd07f60695fdd7ede12e82dc
Eldar <92878793+eldarheleg@users.noreply.github.com>
2024-09-08 13:06:34 +0300

    [ font nav ]

---
+++ app/ui/fonts/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
+
+export const inter = Inter({subsets: ["latin"]});
+export const montserrat = Montserrat({ weight: ["100", "400"], subsets: ["latin"]});
